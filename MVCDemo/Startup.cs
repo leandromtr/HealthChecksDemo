afa8ac21a26ecc78e35ad8f02c24b2e4dabd36d0
leandro <leandromtr@hotmail.com>
2021-04-03 22:16:52 +0100

    Add MVC Demo

--- MVCDemo/Startup.cs
+++ MVCDemo/Startup.cs
@@ -1,20 +1,18 @@
+using HealthChecks.UI.Client;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
 using Microsoft.Extensions.Hosting;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using BlazorDemo.Data;
-using HealthChecks.UI.Client;
 
-namespace HealthChecksDemo
+namespace MVCDemo
 {
     public class Startup
     {
@@ -26,11 +24,9 @@ namespace HealthChecksDemo
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
-            services.AddServerSideBlazor();
+            services.AddControllersWithViews();
 
             // Configure Health Check 
             services.AddHealthChecks()
@@ -38,7 +34,6 @@ namespace HealthChecksDemo
                 .AddCheck("Bar Service", () => HealthCheckResult.Healthy("The check of the bar service worked."), new[] { "service" })
                 .AddCheck("Database", () => HealthCheckResult.Healthy("The check of the Database worked."), new[] { "database", "sql" });
 
-            services.AddSingleton<WeatherForecastService>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,35 +45,27 @@ namespace HealthChecksDemo
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
             app.UseRouting();
 
+            app.UseAuthorization();
+
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapHealthChecks("/quickhealth", new HealthCheckOptions()
-                {
-                    Predicate = _ => false
-                });
-
-                endpoints.MapHealthChecks("/health/services", new HealthCheckOptions()
-                {
-                    Predicate = reg => reg.Tags.Contains("service"),
-                    ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
-                });
-
                 endpoints.MapHealthChecks("/health", new HealthCheckOptions()
                 {
                     ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
                 });
-                endpoints.MapBlazorHub();
-                endpoints.MapFallbackToPage("/_Host");
+
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
             });
         }
     }
